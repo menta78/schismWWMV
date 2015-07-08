964214c479a30bba8a9bd4d7a70c03682f60e4c1
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2015-07-08 10:50:17 +0200

    More copy paste and now support for the wind output.
      Mathieu

--- src/WWMIII/wwm_export_ww3.F90
+++ src/WWMIII/wwm_export_ww3.F90
@@ -30,7 +30,64 @@
       SUBROUTINE EXPORT_WIND_WW3_FORMAT
       USE DATAPOOL
       IMPLICIT NONE
-      
+      CHARACTER(LEN=13) :: TSSTR, IDSTR = 'WAVEWATCH III'
+      INTEGER, PARAMETER :: UNGTYPE = 3
+      LOGICAL, SAVE :: IsFirst = .TRUE.
+      INTEGER :: GTYPE, NX, NY, IX, IY
+      INTEGER :: FILLER(3)
+      INTEGER :: TIDEFLAG = 0
+      INTEGER :: TFN(2)
+      INTEGER :: IP
+      INTEGER TheOut
+      real, allocatable :: Uwr(:,:), Vwr(:,:)
+      real, allocatable :: Uwr_glob(:,:), Vwr_glob(:,:)
+      CHARACTER(LEN=3)  :: IDFLD ='WND'
+      FILLER(:)=0
+      NX=np_total
+      NY=1
+      CALL INIT_TFN(TFN)
+      allocate(Vwr(NX,NY), Uwr(NX,NY), stat=istat)
+      IF (istat/=0) CALL WWM_ABORT('wwm_export_ww3, current part')
+#if defined MPI_PARALL_GRID
+      DO IP=1,MNP
+         Uwr(iplg(IP),1)=MySNGL(WINDXY(IP,1))
+         Vwr(iplg(IP),1)=MySNGL(WINDXY(IP,2))
+      END DO
+      allocate(Vwr_glob(NX,NY), Uwr_glob(NX,NY), stat=istat)
+      call mpi_reduce(Uwr,Uwr_glob,NP_GLOBAL,MPI_REAL, MPI_SUM,0,comm,ierr)
+      call mpi_reduce(Vwr,Vwr_glob,NP_GLOBAL,MPI_REAL, MPI_SUM,0,comm,ierr)
+      IF (myrank == 0) THEN
+        DO IP=1,np_total
+          Uwr(IP,1)=Uwr_glob(IP,1)*nwild_gb(IP)
+          Vwr(IP,1)=Vwr_glob(IP,1)*nwild_gb(IP)
+        END DO
+      END IF
+      deallocate(Vwr_glob, Uwr_glob)
+#else
+      DO IP=1,np_total
+         Uwr(IP,1)=MySNGL(WINDXY(IP,1))
+         Vwr(IP,1)=MySNGL(WINDXY(IP,2))
+      END DO
+#endif
+#if defined MPI_PARALL_GRID
+      IF (myrank == 0) THEN
+#endif
+        TheOut=FHNDL_EXPORT_CURR_WW3
+        IF (IsFirst) THEN
+          OPEN(TheOut, FILE='wind.ww3', FORM='UNFORMATTED', status='new', action='write')
+          WRITE (TheOut) IDSTR, IDFLD, NX, NY, GTYPE, FILLER(1:2), TIDEFLAG
+        ELSE
+          OPEN(TheOut, FILE='wind.ww3', FORM='UNFORMATTED', status='old', position='append', action='write')
+        END IF
+        WRITE (TheOut) TFN
+        WRITE (TheOut) ((Vwr(IX,IY),IX=1,NX),IY=1,NY)
+        WRITE (TheOut) ((Uwr(IX,IY),IX=1,NX),IY=1,NY)
+        CLOSE(TheOut)
+#if defined MPI_PARALL_GRID
+      END IF
+#endif
+      deallocate(Vwr, Uwr)
+      IsFirst=.FALSE.
       END SUBROUTINE
 !**********************************************************************
 !*                                                                    *
