9cf9b4f5be9b7862ea81485da2e096e61050dc55
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2017-02-07 08:23:15 +0100

    Simplification of the code.
      Mathieu

--- src/WWMIII/wwm_aux.F90
+++ src/WWMIII/wwm_aux.F90
@@ -859,6 +859,23 @@
 !**********************************************************************
 !*                                                                    *
 !**********************************************************************
+     SUBROUTINE CONVERT_VS_VD_WWM(IP, VS, VD, IMATRA, IMATDA)
+     USE DATAPOOL
+     IMPLICIT NONE
+     INTEGER, intent(in) :: IP
+     REAL, intent(in) :: VS(MSC*MDC), VD(MSC*MDC)
+     REAL, intent(out) :: IMATRA(MSC,MDC), IMATDA(MSC,MDC)
+     INTEGER :: IS, ID
+     DO IS=1,MSC
+       DO ID=1,MDC
+          IMATRA(IS,ID) = VS(ID + (IS-1) * MDC) / CG(IS,IP)
+          IMATDA(IS,ID) = VD(ID + (IS-1) * MDC)
+       END DO
+     END DO
+     END SUBROUTINE
+!**********************************************************************
+!*                                                                    *
+!**********************************************************************
      SUBROUTINE ONED2TWOD(AC1D,ACLOC)
          USE DATAPOOL
          IMPLICIT NONE
