099089fc189031784555da66ae3b1748d9f45a5c
aron <aaronroland@gxm.de>
2017-08-06 16:47:55 +0200

    update vegiss part

--- src/WWMIII/wwm_vegdiss.F90
+++ src/WWMIII/wwm_vegdiss.F90
@@ -1,67 +1,82 @@
 !****************************************************************
 !
 !****************************************************************
-#ifdef SCHISM
-      SUBROUTINE VEGDISSIP (SDVEG,DDVEG,ACLOC,DEPTH,ETOT,SBAR,KBAR) 
+      SUBROUTINE VEGDISSIP (IP,SSVEG,DSSVEG,ACLOC,DEPTH,ETOT,SBAR,KBAR) 
         USE DATAPOOL
         IMPLICIT NONE
+ 
+        INTEGER, INTENT(IN)     :: IP
+
         REAL(rkind),INTENT(IN)  :: KBAR,SBAR,ETOT,DEPTH
         REAL(rkind),INTENT(IN)  :: ACLOC(NUMSIG,NUMDIR)
-        REAL(rkind),INTENT(OUT) :: SDVEG(NUMSIG,NUMDIR), DDVEG(NUMSIG,NUMDIR)
+        REAL(rkind),INTENT(OUT) :: SSVEG(NUMSIG,NUMDIR), DSSVEG(NUMSIG,NUMDIR)
 
         REAL(rkind) :: BGDISS, KDBAR, SVEGET, VEGDISS
-        REAL(rkind) :: vdrgcoeff_local(nvrt), vdiam_local(nvrt), vdens_local(nvrt), lthick_local(nvrt)
-        INTEGER :: IS, ID
+        INTEGER     :: IS, ID
+
+        IF (ETOT .LT. THR) RETURN
 
         KDBAR  = KBAR * DEPTH
 !
-        BGDISS = SQRT(TWO/PI)*G9**2*(KBAR/SBAR)**3*SQRT(ETOT)/(THREE*KBAR*(COSH(KDBAR))**3)!*NPLANTSPSQM(IP)
+        BGDISS = SQRT(TWO/PI)*G9**2*(KBAR/SBAR)**3*SQRT(ETOT)/(THREE*KBAR*(COSH(KDBAR))**3)*NPLANTSPSQM(IP)
 !
-!2do ... extract from schism 
-
 !        vdrgcoeff_local =  ! drag coefficient / per layer and node 
 !        vdiam_local     =  ! diam of veg. / per layer and node 
 !        vdens_local     =  ! veg. density / per layer and node 
 !        lthick_local    =  ! lay. thickness / per layer and node 
 !
-        CALL intvegdissip(vegdiss,nvrt,vdrgcoeff_local,vdiam_local,vdens_local,kbar,lthick_local) 
-        
-        SVEGET = BGDISS + VEGDISS 
-    
+        vegdiss(:,IP)   = 0
+        vdrgcoeff(:,IP) = 1.
+        vdiam(:,IP)     = 0.04
+        vdens(:,IP)     = 0.6 
+        lthick(:,IP)    = 2.
+
+        CALL INTVEGDISSIP(vegdiss,nvrt,dep(ip),kbar,vdrgcoeff(:,IP),vdiam(:,IP),vdens(:,IP),lthick(:,IP)) 
+
+        SVEGET = BGDISS * VEGDISS 
+
         DO IS = 1, NUMSIG
           DO ID = 1, NUMDIR
-            DDVEG(IS,ID) = - SVEGET
-            SDVEG(IS,ID) = - SVEGET * ACLOC(IS,ID)
+            DSSVEG(IS,ID) = - SVEGET
+            SSVEG(IS,ID)  = - SVEGET * ACLOC(IS,ID)
           ENDDO
         ENDDO 
-
+ 
       END SUBROUTINE
 !****************************************************************
 !
 !****************************************************************
-      subroutine intvegdissip(vegdiss,nlay,vdrgcoeff,vdiam,vdens,kbar,lthick)
+      subroutine intvegdissip(vegdiss,nlay,depth,kbar,vdrgcoeff,vdiam,vdens,lthick)
         USE DATAPOOL, ONLY : RKIND
         implicit none
+
+        real(rkind),intent(in)  :: depth
         real(rkind),intent(in)  :: kbar
         real(rkind),intent(in)  :: vdrgcoeff(nlay)
         real(rkind),intent(in)  :: vdiam(nlay)
         real(rkind),intent(in)  :: vdens(nlay)
         real(rkind),intent(in)  :: lthick(nlay)
         real(rkind),intent(out) :: vegdiss
+        real(rkind)             :: svkh1, svkh2, coeff, kvh
+
         integer,intent(in)      :: nlay
         integer                 :: i,j
-        real(rkind)             :: svkh1, svkh2, sum_thick, coeff
+
         svkh1 = 0.d0
         svkh2 = 0.d0
-        sum_thick = 0.d0
+        kvh   = 0.d0
         do i = 1, nlay
-          svkh1            = svkh1 + sinh(kbar * lthick(i-1))
-          if(i.gt.1) svkh2 = svkh2 + sinh(kbar * lthick(i))
-          coeff            = (svkh2**3-svkh1**3)+3*(svkh2-svkh1)
-          vegdiss          = vegdiss + coeff*vdiam(i)*vdens(i)*lthick(i)
+          sumlay  = sumlay + lthick(i)  
+          if (vdiam(i) .gt. ZERO) then
+            kvh     = kvh + kbar * lthick(i)
+            svkh1   = svkh2 
+            svkh2   = svkh2 + sinh(kvh)
+            coeff   = (svkh2**3-svkh1**3)+3*(svkh2-svkh1)
+            vegdiss = vegdiss + coeff*vdiam(i)*vdens(i)*lthick(i)
+          endif
         enddo
+
       end subroutine
-#endif
 !****************************************************************
 !
 !****************************************************************
