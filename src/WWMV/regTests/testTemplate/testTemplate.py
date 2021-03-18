a955e7183106d132a44b7347fd9dd526558b720c
Lorenzo Mentaschi <lorenzo.mentaschi@unige.it>
2021-03-18 19:08:27 +0100

    menta: wwm test skeleton

---
+++ src/WWMV/regTests/testTemplate/testTemplate.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class testRegTest(unittest.TestCase):
+
+  def getCurrentPath(self):
+    pass
+
+  def setUp(self):
+    pass
+
+  def test0(self):
+    # this test verifies that the architecture works
+    self.assertTrue(True)
+
+
+if __name__ == '__main__':
+  unittest.main()
