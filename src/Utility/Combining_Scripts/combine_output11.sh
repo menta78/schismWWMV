23e6228293aae6aabb07408bb82f4b2612ea05cc
Lorenzo Mentaschi <lmentasc@login01.leonardo.local>
2025-03-14 17:21:15 +0100

    utility scripts to compile and launch the combine fortran codes on the leonardo hpc

---
+++ src/Utility/Combining_Scripts/combine_output11.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+callpth=${BASH_SOURCE[0]}
+callpth=$(readlink -f $callpth)
+bashsrcdir=$(cd `dirname "$callpth"` && pwd)
+
+module load netcdf-fortran/4.6.1--gcc--12.2.0 # should also load te dependent modules
+
+$bashsrcdir/combine_output11 $@
+
---
+++ src/Utility/Combining_Scripts/menta_compile_combine_hotstart7.sh
@@ -0,0 +1,6 @@
+module purge
+
+module load gcc/12.2.0
+module load netcdf-fortran/4.6.1--gcc--12.2.0 # should also load te dependent modules
+
+gfortran -O3 -Bstatic -o combine_hotstart7 -ffree-line-length-none ../UtilLib/schism_geometry.f90 ../UtilLib/argparse.f90 combine_hotstart7.f90 -I/leonardo/prod/spack/5.2/install/0.21/linux-rhel8-icelake/gcc-12.2.0/netcdf-fortran-4.6.1-ioyufgaugvqjspshyaje65wscwubpvpz/include/ -I/leonardo/prod/spack/5.2/install/0.21/linux-rhel8-icelake/gcc-12.2.0/netcdf-c-4.9.2-wk7h7wank6kyxvcq7gru5taerb2dec3t/include -lnetcdff -lnetcdf
